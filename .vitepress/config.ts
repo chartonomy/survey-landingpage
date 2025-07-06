11983a9ceb40f82920f36bc598dc54efd68bbea2
jankapunkt <jkuester@uni-bremen.de>
2025-07-06 13:02:23 +0200

    build: set proper base path in config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "selbsttest.org",
+  base: '/survey-landingpage/',
   description: "Verlässliche anonyme Selbsttests für Ihr Team",
   sitemap: {
     hostname: 'https://selbsttest.org'
